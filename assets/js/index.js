6d012e1a9fb5c929fef3f5c04afecc65789e5475
SheeTabz <wmuriithitabitha@gmail.com>
2022-08-31 10:39:09 +0300

    Function that handles ratings

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,18 +11,60 @@ document.addEventListener("DOMContentLoaded", () => {
                 function appendsOnClick(filtered) {
                     card.innerHTML = ''
                     filtered.map(hom => {
+                        let ratings;
+                        console.log(hom.rating)
+                        if(hom.rating >=9.8){
+                            ratings = `<label for="">Ratings:
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                        </label>`
+                        }
+                        else if(hom.rating >=9.3 && hom.rating <=9.7){
+                            ratings =`<label for="">Ratings:
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star "></span>
+                        </label>`
+                        }
+                        else if(hom.rating >=9.0 && hom.rating <=9.2){
+                          ratings =  `<label for="">Ratings:
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star "></span>
+                            <span class="fa fa-star "></span>
+                        </label>`
+                        }
+                        else if(hom.rating >=2 && hom.rating <=8.9){
+                            ratings =  `<label for="">Ratings:
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star "></span>
+                            <span class="fa fa-star "></span>
+                            <span class="fa fa-star "></span>
+                        </label>`
+                        }
+                        else {
+                            ratings =  `<label for="">Ratings:
+                            <span class="fa fa-star checked"></span>
+                            <span class="fa fa-star "></span>
+                            <span class="fa fa-star "></span>
+                            <span class="fa fa-star "></span>
+                            <span class="fa fa-star "></span>
+                        </label>`
+                        }
                         const item = document.createElement("div")
                         item.setAttribute("class", "card")
                         item.innerHTML = ` <img src="${hom.img_cover}" alt="">
                           <h3 class="property-type">${hom.property_type}</h2>
                               <p>${hom.title}</p>
-                              <label for="">Ratings:
-                                  <span class="fa fa-star checked"></span>
-                                  <span class="fa fa-star checked"></span>
-                                  <span class="fa fa-star checked"></span>
-                                  <span class="fa fa-star"></span>
-                                  <span class="fa fa-star"></span>
-                              </label>`
+                              
+                              ${ratings}`
                         card.appendChild(item)
 
                     })
@@ -52,6 +94,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 function appendVacationHomes(data) {
                     const filtered = data.filter(item => item.property_type == "Villa" ? item : null)
                     const vacBtn = document.getElementById("vacation")
+                    console.log()
                     eventListener(vacBtn, filtered)
                 }
                 appendVacationHomes(properties)
@@ -86,6 +129,20 @@ document.addEventListener("DOMContentLoaded", () => {
             })
     }
     fetchData()
+
+    const form = document.querySelector("form")
+    console.log(form)
+    form.addEventListener("submit", (e)=>{
+      e.preventDefault()
+      const inputArea = form.comments.value
+      console.log(inputArea)
+      const ul = document.getElementById("response")
+      const li = document.createElement("li")   
+      li.textContent = inputArea
+      ul.appendChild(li)
+
+    form.reset()
+    })
 })
 
 
